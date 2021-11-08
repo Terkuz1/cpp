cd36ed68a203494b66523f6c634f063f9029982e
Terkuz1 <93916768+Terkuz1@users.noreply.github.com>
2021-11-08 12:16:42 +0100

    Create silnia.cpp

---
+++ silnia.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    int ile;
+    int n[1000];
+    int liczba;
+
+    cin>>ile;
+    for(int i=0;i<=n[ile-1];i++){
+        cin>>n[i];
+    }
+    for(int i=0;i<=n[ile-1];i++){
+        cout<<n[i];
+    }
+
+    return 0;
+}
